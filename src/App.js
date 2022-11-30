03b15f4f629c263939b35ee86487093213314f91
Redonearth <wona23@gmail.com>
2022-11-30 20:57:49 +0900

    feat: 할 일 추가하기 구현

--- src/App.js
+++ src/App.js
@@ -1,7 +1,48 @@
-import './App.css';
+import { useState } from 'react';
+import styles from './App.module.css';
 
-function App() {
-  return <div>Hello</div>;
+export default function App() {
+  const [toDos, setToDos] = useState(initialToDos);
+  const [toDoText, setToDoText] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const handleChangeText = (e) => {
+    setToDoText(e.target.value);
+  };
+
+  const handleAddToDo = () => {
+    setToDos((prev) => [
+      { id: prev.length + 1, content: toDoText, isDone: false },
+      ...prev,
+    ]);
+  };
+
+  return (
+    <div className={styles.appContainer}>
+      <form onSubmit={handleSubmit}>
+        <ul>
+          {toDos.map((toDo) => (
+            <li key={toDo.id}>
+              <span>{toDo.content}</span>
+            </li>
+          ))}
+        </ul>
+        <input
+          type="text"
+          placeholder="할 일 작성하기"
+          value={toDoText}
+          onChange={handleChangeText}
+        />
+        <button onClick={handleAddToDo}>추가</button>
+      </form>
+    </div>
+  );
 }
 
-export default App;
+const initialToDos = [
+  { id: 1, content: '강의 보기', isDone: false },
+  { id: 2, content: '청소하기', isDone: false },
+];
